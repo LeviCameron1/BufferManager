f6ae48e54a8cbf4d856a4b6c2554b6078a3f97b6
Levi Cameron <lcameron3@wisc.edu>
2022-12-12 23:27:23 -0600

    Implemented QU_Delete() in delete.c

--- delete.C
+++ delete.C
@@ -14,13 +14,30 @@ const Status QU_Delete(const string & relation,
 		       const string & attrName, 
 		       const Operator op,
 		       const Datatype type, 
-		       const char *attrValue)
+		       const char *attrValue) // relname.attr
 {
-// part 6
+Status status;
+HeapFileScan scan(relation, status);
+if(status != OK) return status;
+// Check if input argument is null
+	if(attrName[0] == '\0'){
+		status = scan.startScan(0,0,STRING,NULL,op);
+	}
+	else{ // if input is not null get description of the attribute
+		AttrDesc attrDesc;
+		attrCat->getInfo(relation,attrName,attrDesc);
+		status = scan.startScan(attrDesc.attrOffset,attrDesc.attrLen,type,attrValue,op);
+	}
+
+	RID tmpRID;
+	// go through the heap file deleting records that are a match
+	while (scan.scanNext(tmpRID) == OK)
+    {
+		status = scan.deleteRecord();
+		assert(status == OK);
+	}
+
 return OK;
-
-
-
 }
 
 
